2efd7e24ca132f7ba49323a114bcdf2e2a333b7d
devikavps <devikavps@gmail.com>
2026-06-01 20:28:45 +0530

    Update meal_bot.py

--- meal_bot.py
+++ meal_bot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # Direct API Key
-GOOGLE_API_KEY = "AIzaSyDS0pHfF3_1-DK5CmqB68Fy2nVpuNiFHiw"
+GOOGLE_API_KEY = "AIzaSyCZT9WYwhIeeJqlsGqClYTV5W3FF81AoWc"
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
